22b21fad6d12fd21a9ab23afdf825a0143b36d39
Fabian Orozco <fabian.orozco@mercadolibre.com.co>
2023-11-01 21:58:34 -0500

    Feasture: Fronts

--- app/src/main/java/com/example/myapplication/model/models/Album.kt
+++ app/src/main/java/com/example/myapplication/model/models/Album.kt
@@ -1,12 +1,13 @@
 package com.example.myapplication.model.models
 
 import com.example.myapplication.utils.DateUtil
+import com.example.myapplication.utils.ImageUtil
 import org.json.JSONObject
 
-data class Album (
+data class Album(
     val albumId:Int,
     val name:String,
-    val cover:String,
+    val cover: Comparable<*>,
     val releaseDate:String,
     val description:String,
     val genre:String,
@@ -15,7 +16,7 @@ data class Album (
     constructor(albumId: JSONObject?) : this(
         albumId?.optInt("id") ?: 0,
         albumId?.optString("name") ?: "",
-        albumId?.optString("cover") ?: "",
+        ImageUtil.getImage(albumId?.optString("cover") ?: ""),
         DateUtil.format(albumId?.optString("releaseDate") ?: ""),
         albumId?.optString("description") ?: "",
         albumId?.optString("genre") ?: "",
