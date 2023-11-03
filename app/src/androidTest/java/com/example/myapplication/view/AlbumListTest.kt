ca5d03a6f1b6e7832fc36d2bbaedbf0439e19cb5
snader21 <s.nader21@uniandes.edu.co>
2023-11-02 22:23:00 -0500

    Se crea una primera versión del test e2e sobre la lista de albums

---
+++ app/src/androidTest/java/com/example/myapplication/view/AlbumListTest.kt
@@ -0,0 +1,93 @@
+package com.example.myapplication.view
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.myapplication.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.navigation_artist), withContentDescription("Artistas"),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val bottomNavigationItemView2 = onView(
+            allOf(
+                withId(R.id.navigation_album_list), withContentDescription("Albumes"),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView2.perform(click())
+
+        onView(withId(R.id.albumRv))
+            .check(matches(atPosition(0, hasDescendant(withText("Buscando América")))))
+
+        onView(withId(R.id.albumRv))
+            .check(matches(atPosition(0, hasDescendant(withText("1984-08-01")))))
+
+        onView(withId(R.id.albumRv))
+            .check(matches(atPosition(0, hasDescendant(withText("Salsa")))))
+
+        onView(withId(R.id.albumRv))
+            .check(matches(atPosition(1, hasDescendant(withText("Poeta del pueblo")))))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+    fun atPosition(position: Int, itemMatcher: Matcher<View>): Matcher<View> {
+        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
+            }
+
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val viewHolder = view.findViewHolderForAdapterPosition(position) ?: return false
+                return itemMatcher.matches(viewHolder.itemView)
+            }
+        }
+    }
+}
