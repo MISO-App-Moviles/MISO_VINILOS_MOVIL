e14939ddeb92d3db7d7dcfe30f0b0373c2243a81
Ricardo Vivas <j.vivast@uniandes.edu.co>
2023-11-03 17:56:49 -0500

    Se crea logica para lista de colleccionistas

--- app/src/main/java/com/example/myapplication/model/models/Collector.kt
+++ app/src/main/java/com/example/myapplication/model/models/Collector.kt
@@ -1,8 +1,19 @@
 package com.example.myapplication.model.models
 
+import com.example.myapplication.utils.DateUtil
+import com.example.myapplication.utils.ImageUtil
+import org.json.JSONObject
+
 data class Collector (
     val collectorId: Int,
     val name:String,
     val telephone:String,
     val email:String,
-)
+){
+    constructor(collectorId: JSONObject?) : this(
+        collectorId?.optInt("id") ?: 0,
+        collectorId?.optString("name") ?: "",
+        collectorId?.optString("telephone") ?: "",
+        collectorId?.optString("email") ?: "",
+    )
+}
