660294e42f4426932ffa9e4a88f12645da5f3897
snader21 <s.nader21@uniandes.edu.co>
2023-11-03 19:13:19 -0500

    Refactor del test de la lista de albums

--- app/src/androidTest/java/com/example/myapplication/view/AlbumListTest.kt
+++ app/src/androidTest/java/com/example/myapplication/view/AlbumListTest.kt
@@ -1,93 +1,41 @@
 package com.example.myapplication.view
 
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.myapplication.R
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
+import com.example.myapplication.view.utils.EspressoTestUtils.clickBottomNavigationItem
+import com.example.myapplication.view.utils.EspressoTestUtils.checkItemInRecyclerView
+import org.junit.Test
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest {
+class AlbumListTest {
+
+    companion object {
+        const val NAVIGATION_ARTIST_TAB = "Artistas"
+        const val NAVIGATION_ALBUM_TAB = "Albumes"
+        const val ALBUM_NAME = "Buscando América"
+        const val RELEASE_DATE = "1984-08-01"
+        const val GENRE = "Salsa"
+    }
 
     @Rule
     @JvmField
     var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
-
     @Test
-    fun mainActivityTest() {
-        val bottomNavigationItemView = onView(
-            allOf(
-                withId(R.id.navigation_artist), withContentDescription("Artistas"),
-                isDisplayed()
-            )
-        )
-        bottomNavigationItemView.perform(click())
+    fun testFirstAlbumDetails() {
+        clickBottomNavigationItem(R.id.navigation_artist, NAVIGATION_ARTIST_TAB)
+        clickBottomNavigationItem(R.id.navigation_album_list, NAVIGATION_ALBUM_TAB)
 
-        val bottomNavigationItemView2 = onView(
-            allOf(
-                withId(R.id.navigation_album_list), withContentDescription("Albumes"),
-                isDisplayed()
-            )
-        )
-        bottomNavigationItemView2.perform(click())
-
-        onView(withId(R.id.albumRv))
-            .check(matches(atPosition(0, hasDescendant(withText("Buscando América")))))
-
-        onView(withId(R.id.albumRv))
-            .check(matches(atPosition(0, hasDescendant(withText("1984-08-01")))))
-
-        onView(withId(R.id.albumRv))
-            .check(matches(atPosition(0, hasDescendant(withText("Salsa")))))
-
-        onView(withId(R.id.albumRv))
-            .check(matches(atPosition(1, hasDescendant(withText("Poeta del pueblo")))))
-    }
-
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
-    }
-
-    fun atPosition(position: Int, itemMatcher: Matcher<View>): Matcher<View> {
-        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
-            override fun describeTo(description: Description) {
-                description.appendText("has item at position $position: ")
-                itemMatcher.describeTo(description)
-            }
-
-            override fun matchesSafely(view: RecyclerView): Boolean {
-                val viewHolder = view.findViewHolderForAdapterPosition(position) ?: return false
-                return itemMatcher.matches(viewHolder.itemView)
-            }
-        }
+        checkItemInRecyclerView(R.id.albumRv, 0, R.id.imageView)
+        checkItemInRecyclerView(R.id.albumRv, 0, R.id.textView6, ALBUM_NAME)
+        checkItemInRecyclerView(R.id.albumRv, 0, R.id.textView, RELEASE_DATE)
+        checkItemInRecyclerView(R.id.albumRv, 0, R.id.textView5, GENRE)
     }
 }
