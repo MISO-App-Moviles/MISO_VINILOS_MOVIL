8f577a04443c097ac5f82bc67ae472b2ff55691d
Fabian Orozco <fabian.orozco@mercadolibre.com.co>
2023-11-04 11:31:13 -0500

    Fix:function

--- app/src/main/java/com/example/myapplication/utils/util.kt
+++ app/src/main/java/com/example/myapplication/utils/util.kt
@@ -7,8 +7,12 @@ object DateUtil {
     private val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
     private val outputFormat = SimpleDateFormat("yyyy-MM-dd")
     fun format(dateString: String): String {
-        val date = inputFormat.parse(dateString)
-        return outputFormat.format(date)
+        try {
+            val date = inputFormat.parse(dateString)
+            return outputFormat.format(date)
+        } catch (e: Exception) {
+            return "2023-11-04"
+        }
     }
 }
 
