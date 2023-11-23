f6e464c4e282774507bac7e641c5c42bdd7c12af
Andres Martinez <af.martinezc1@uniandes.udu.co>
2023-11-22 23:48:14 -0500

    Style view adjusted and mask applied

--- app/src/main/java/com/example/myapplication/view/AddAlbumFragment.kt
+++ app/src/main/java/com/example/myapplication/view/AddAlbumFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.example.myapplication.R
 import com.example.myapplication.databinding.AddAlbumFragmentBinding
 import com.example.myapplication.databinding.AlbumFragmentBinding
@@ -28,12 +32,23 @@ class AddAlbumFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.editTextCover.doAfterTextChanged {
+            setImage(view, it.toString())
+            Log.d("Tag",  it.toString())
+        }
         binding.submitButton.setOnClickListener {
             val albumName = binding.editTextName.text
             Log.d("Tag",  albumName.toString())
             binding.editTextName.setText("")
             view.findNavController().navigateUp()
         }
+    }
+
+    fun setImage(view: View, url: String){
+        Glide.with(view.getContext())
+            .load(url)
+            .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL))
+            .into(binding.albumPreviewImage);
     }
 
 }
